ee913a37e797c2f8253fd8568ed21b2a825a6671
che30 <che.blancoo58@gmail.com>
2020-11-05 20:09:18 +0100

    add regular expression features

--- main.rb
+++ main.rb
@@ -4,51 +4,83 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
 
-   arr = self
-    counter = 0
-    loop do
-      yield arr[counter]
-      counter += 1
-      break if counter == arr.length
-    end
-  end
-  def my_each_with_index
-    return to_enum(:my_each) unless block_given?
     arr = self
     counter = 0
     loop do
-      yield(arr[counter], counter)
+      yield arr.to_a[counter]
       counter += 1
-      break if counter == arr.length
+      break if counter == arr.to_a.length
     end
   end
-  
+
+  def my_each_with_index
+    return to_enum(:my_each) unless block_given?
+
+    arr = self
+    counter = 0
+    loop do
+      yield(arr.to_a[counter], counter)
+      counter += 1
+      break if counter == arr.to_a.length
+    end
+  end
+
   def my_select
     return to_enum(:my_select) unless block_given?
-    
-   arr = self
+
+    arr = self
     counter = 0
-    selected_elements = []
-    until counter ==arr.length
-    selected_elements <<arr[counter] if yield(arr[counter])
-    counter += 1
+    selected_elts = []
+    until counter == arr.length
+      selected_elts << arr[counter] if yield(arr[counter])
+      counter += 1
     end
-    p selected_elements
-    end
-    def my_all?(word = nil)
-        return to_enum(:my_all) unless block_given?
-        return false if word == ''
-    
-        my_each do |element|
-          return false unless yield(element) == true
+    p selected_elts
+  end
+
+  def my_all?(word = nil)
+    if block_given?
+        my_each do |elt|
+            return false unless yield(elt) == true
         end
         true
+    elsif word.nil?
+        my_each { |elt| return false if elt.nil? || elt == false }
+      elsif !arg.nil? && (arg.is_a? Class)
+        my_each { |elt| return false if elt.class != arg }
+      elsif !arg.nil? && arg.class == Regexp
+        my_each { |elt| return false unless arg.match(elt) }
+      else
+        my_each { |elt| return false if elt != arg }
       end
-      def my_any?
-        my_each do |element|
-        return true unless yield(element) == false
+      true
+    end
+  end
+
+  def my_any?(word=nil)
+    if block_given?
+        my_each do |elt|
+            return true unless yield(elt) == false
         end
-        # p 'testing with any '
         false
-        end 
+    elsif word.nil?
+        my_each { |elt| return true if elt.nil? || elt == true }
+    elsif !arg.nil? && (arg.is_a? Class)
+        my_each { |elt| return true if elt.class != arg }
+    elsif !arg.nil? && arg.class == Regexp
+        my_each { |elt| return true unless arg.match(elt) }
+    else
+        my_each { |elt| return true if elt != arg }
+    end
+    false
+end
+end
+[1, 2, 3, 4].my_each do |element|
+  puts element
+end
+[3, 1, 3, 2].my_each_with_index do |element, index|
+  print element, index
+end
+(1..4).my_each_with_index do |element, index|
+  print element, index
 end

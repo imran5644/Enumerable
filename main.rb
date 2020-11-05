20abc454259c58f33e75343a37e78407994e16d2
che30 <che.blancoo58@gmail.com>
2020-11-05 18:54:08 +0100

    implement my_all

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
 
-    array = self
+   arr = self
     counter = 0
     loop do
       yield arr[counter]
@@ -26,13 +26,22 @@ module Enumerable
   def my_select
     return to_enum(:my_select) unless block_given?
     
-    array = self
+   arr = self
     counter = 0
     selected_elements = []
-    until counter == array.length
-    selected_elements << array[counter] if yield(array[counter])
+    until counter ==arr.length
+    selected_elements <<arr[counter] if yield(arr[counter])
     counter += 1
     end
     p selected_elements
     end
+    def my_all?(word = nil)
+        return to_enum(:my_all) unless block_given?
+        return false if word == ''
+    
+        my_each do |element|
+          return false unless yield(element) == true
+        end
+        true
+      end
 end

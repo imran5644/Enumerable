adf17eb68187d22dd4304d8e3d21590358792e6d
che30 <che.blancoo58@gmail.com>
2020-11-05 18:45:55 +0100

    implement my_each_with_index

--- main.rb
+++ main.rb
@@ -1,13 +1,25 @@
-module Enumerable
-    def my_each
-        return to_enum(:my_each) unless block_given?
+# frozen_string_literal: true
 
-        array = self
-        counter = 0
-        loop do
-        yield array[counter]
-        counter += 1
-        break if counter == array.length
-        end 
+module Enumerable
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    array = self
+    counter = 0
+    loop do
+      yield arr[counter]
+      counter += 1
+      break if counter == arr.length
     end
-    end 
+  end
+  def my_each_with_index
+    return to_enum(:my_each) unless block_given?
+    arr = self
+    counter = 0
+    loop do
+      yield(arr[counter], counter)
+      counter += 1
+      break if counter == arr.length
+    end
+  end
+end

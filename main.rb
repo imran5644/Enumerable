56071bcca29a579f7b3a3446c53528300b42c5b9
che30 <che.blancoo58@gmail.com>
2020-11-06 22:14:25 +0100

    implement changesof TSE

--- main.rb
+++ main.rb
@@ -40,35 +40,34 @@ module Enumerable
 
   def my_all?(arg = nil)
     if block_given?
-      my_each do |elt|
-        return false unless yield(elt) == true
-      end
-      true
-    elsif arg.nil?
-      my_each { |elt| return false if elt.nil? || elt == false }
-    elsif !arg.nil? && (arg.is_a? Class)
-      my_each do |elt|
-        return true unless elt.instance_of?(arg.class)
-      end
-      false
-    elsif !arg.nil? && arg.instance_of?(Regexp)
-      my_each { |elt| return false unless elt.match(arg) }
-      true
+      my_each { |elt| return false if yield(elt) }
+      return true
+    end
+    arg.nil? ? arg.class.to_s : my_all? { |elt| elt }
+
+    case arg.class.to_s
+    when 'Class'
+      my_all? { |elt| elt.is_a? arg }
+    when 'Regexp'
+      my_all? { |elt| elt =~ arg }
+    else
+      my_all? { |elt| elt == arg }
     end
   end
 
   def my_any?(arg = nil)
     if block_given?
       my_each { |elt| return true if yield(elt) }
-      false
+      return false
+    end
+    arg.nil? ? arg.class.to_s : my_any? { |elt| elt }
+
+    if arg.class.to_s == 'Class'
+      my_each { |elt| return true if elt.is_a? arg }
+    elsif arg.class.to_s == 'Regexp'
+      my_each { |elt| return true if elt =~ arg }
     elsif arg.nil?
-      my_each { |elt| return true if elt.nil? || elt == true }
-    elsif !arg.nil? && (arg.is_a? Class)
-      my_each { |elt| return true unless elt.class != arg }
-      false
-    elsif !arg.nil? && (arg.instance_of? Regexp)
-      my_each { |elt| return true if arg.match(elt) }
-      false
+      my_each { |elt| return true if elt }
     else
       my_each { |elt| return true if elt == arg }
     end
@@ -112,59 +111,107 @@ module Enumerable
     new_arr
   end
 end
-
-def my_inject(num = nil, sym = nil)
-  if block_given?
-    memo = num
-    my_each do |elt|
-      memo = memo.nil? ? elt : yield(memo, elt)
-    end
-    memo
-  elsif !num.nil? && (num.is_a?(Symbol) || num.is_a?(String))
-    memo = nil
-    my_each do |elt|
-      memo = memo.nil? ? elt : memo.send(num, elt)
-    end
-    memo
-  elsif !sym.nil? && (sym.is_a?(Symbol) || sym.is_a?(String))
-    memo = num
-    my_each do |elt|
-      memo = memo.nil? ? elt : memo.send(sym, elt)
-    end
-    memo
-
+def my_inject(arg = nil, sym = nil)
+  if (arg.is_a?(Symbol) || arg.is_a?(String)) && (!arg.nil? && sym.nil?)
+    sym = arg
+    arg = nil
   end
+
+  if !block_given? && !sym.nil?
+    my_each { |elt| arg = arg.nil? ? elt : arg.send(sym, elt) }
+  else
+    my_each { |elt| arg = arg.nil? ? elt : yield(arg, elt) }
+  end
+  arg
 end
-[1, 2, 3, 4].my_each do |element|
-  puts element
+
+def multiply_els(elts)
+  elts.my_inject { |result, elt| result * elt }
 end
-[3, 1, 3, 2].my_each_with_index do |element, index|
-  puts element
-  print index
-end
-(1..4).my_each_with_index do |element, index|
-  puts element
-  print index
-  puts ''
-end
-p [1, 2, 3, 4, 5].my_select(&:even?)
-p1 = %w[ant bear cat].my_any? { |wor| wor.length >= 3 }
-p2 = [nil, true, 99].my_any?(Integer)
-p3 = %w[ant bear cat].my_any?(/bos/)
-p4 = %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
-p5 = [1, 2i, 3.14].my_all?(Numeric)
-p6 = %w[ant bear cat].my_all?(/t/)
-p7 = [2, 3, 56, 6, 3, 2, 9, 1, 2, 3, 3, 5].my_count
-p8 = (0..5).my_count(2)
-p9 = %w[ant bear cat].my_none? { |word| word.length == 5 }
-p10 = %w[ant bear cat].my_none? { |word| word.length >= 4 }
-puts p1
-puts p2
-puts p3
-puts p4
-puts p5
-puts p6
-puts p7
-puts p8
-puts p9
-puts p10
+# 1. my_each
+puts 'my_each'
+puts '-------'
+puts [1, 2, 3].my_each { |elem| print "#{elem + 1} " }
+# => 2 3 4
+puts
+# 2. my_each_with_index
+puts 'my_each_with_index'
+puts '------------------'
+print [1, 2, 3].my_each_with_index { |elem, idx| puts "#{elem} : #{idx}" }
+# => 1 : 0, 2 : 1, 3 : 2
+puts
+# 3. my_select
+puts 'my_select'
+puts '---------'
+p [1, 2, 3, 8].my_select(&:even?) # => [2, 8]
+p [0, 2018, 1994, -7].my_select(&:positive?) # => [2018, 1994]
+p [6, 11, 13].my_select(&:odd?) # => [11, 13]
+puts
+# 4. my_all? (example test cases)
+puts 'my_all?'
+puts '-------'
+p [3, 5, 7, 11].my_all?(&:odd?) # => true
+p [-8, -9, -6].my_all?(&:negative?) # => true
+p [3, 5, 8, 11].my_all?(&:odd?) # => false
+p [-8, -9, -6, 0].my_all?(&:negative?) # => false
+# test cases required by tse reviewer
+p [1, 2, 3, 4, 5].my_all?(3) # => false
+p [1, 2.5, 'a', 3].my_all?(Integer) # => false
+p %w[dog door rod blade].my_all?(/o/) # => false
+p [1, 1, 1].my_all?(1) # => true
+puts
+# 5. my_any? (example test cases)
+puts 'my_any?'
+puts '-------'
+p [7, 10, 3, 5].my_any?(&:even?) # => true
+p [7, 10, 4, 5].my_any?(&:even?) # => true
+p %w[q r s i].my_any? { |char| 'aeiou'.include?(char) } # => true
+p [7, 11, 3, 5].my_any?(&:even?) # => false
+p %w[q r s t].my_any? { |char| 'aeiou'.include?(char) } # => false
+# test cases required by tse reviewer
+p [nil, false, nil, false].my_any? # => false
+p [1, nil, false].my_any?(Integer) # => true
+p %w[dog door rod blade].my_any?(/z/) # => false
+p [1, 2, 3].my_any?(1) # => true
+puts
+# 6. my_none? (example test cases)
+puts 'my_none?'
+puts '--------'
+p [3, 5, 7, 11].my_none?(&:even?) # => true
+p %w[sushi pizza burrito].my_none? { |word| word[0] == 'a' } # => true
+p [3, 5, 4, 7, 11].my_none?(&:even?) # => false
+p %w[asparagus sushi pizza apple burrito].my_none? { |word| word[0] == 'a' } # => false
+# test cases required by tse reviewer
+p [nil, false, nil, false].my_none? # => true
+p [1, 2, 3].my_none? # => false
+p [1, 2, 3].my_none?(String) # => true
+p [1, 2, 3, 4, 5].my_none?(2) # => false
+p [1, 2, 3].my_none?(4) # => true
+puts
+# 7. my_count (example test cases)
+puts 'my_count'
+puts '--------'
+p [1, 4, 3, 8].my_count(&:even?) # => 2
+p %w[DANIEL JIA KRITI dave].my_count { |s| s == s.upcase } # => 3
+p %w[daniel jia kriti dave].my_count { |s| s == s.upcase } # => 0
+# test cases required by tse reviewer
+p [1, 2, 3].my_count # => 3
+p [1, 1, 1, 2, 3].my_count(1) # => 3
+puts
+# 8. my_map
+puts 'my_map'
+puts '------'
+p [1, 2, 3].my_map { |n| 2 * n } # => [2,4,6]
+p %w[Hey Jude].my_map { |word| "#{word}?" } # => ["Hey?", "Jude?"]
+p [false, true].my_map(&:!) # => [true, false]
+my_proc = proc { |num| num > 10 }
+p [18, 22, 5, 6].my_map(my_proc) { |num| num < 10 } # => true true false false
+puts
+# 9. my_inject
+puts 'my_inject'
+puts '---------'
+p [1, 2, 3, 4].my_inject(10) { |accum, elem| accum + elem } # => 20
+p [1, 2, 3, 4].my_inject { |accum, elem| accum + elem } # => 10
+p [5, 1, 2].my_inject('+') # => 8
+p (5..10).my_inject(2, :*) # should return 302400
+p(5..10).my_inject(4) { |prod, n| prod * n } # should return

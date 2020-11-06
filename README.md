d0d8f8a7b5640460fd65c7cec8aa5ce9bbec9f83
che30 <che.blancoo58@gmail.com>
2020-11-06 17:25:42 +0100

    updated enumarbles,updated readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Enumerable
+# Enumerable# Bubble_sort
+    This project is the second project in the ruby module in microverse. The  project aims at reimplementing  some basic ruby enumarables
+## GETTING STARTED
+    To have a local version of this code running do the following:
+- clone the repository
+- install the ruby compiler and any preferred code editor of your choice
+- Run the command ruby main.rb 
+
+## Built With
+
+- Ruby
+
+## Live Demo
+
+[Live Demo Link](https://repl.it/repls/JumpyObviousNaturaldocs#main.rb)
+
+## Authors
+
+**Che Blanchard**
+
+- GitHub: [@che30](https://github.com/che30)
+- Twitter: [@BlanchardNsoh](https://twitter.com/che55085128 )
+- LinkedIn: [Che Blanchard](https://www.linkedin.com/in/che-nsoh-9455271b0/)
+
+**Imran yousaf**
+
+- GitHub: [@imran5644](https://github.com/imran5644)
+- Twitter: [@imran56444](https://twitter.com/imran56444)
+- LinkedIn: [Imran yousaf](https://www.linkedin.com/in/imran-yousaf-8777297b/)
+
+##  Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## License
+
+This project is [MIT](./LICENSE.txt) licensed.
--- main.rb
+++ main.rb
@@ -35,110 +35,136 @@ module Enumerable
       selected_elts << arr[counter] if yield(arr[counter])
       counter += 1
     end
-    p selected_elts
+    selected_elts
   end
 
-  def my_all?(word = nil)
+  def my_all?(arg = nil)
     if block_given?
-        my_each do |elt|
-            return false unless yield(elt) == true
-        end
-        true
-    elsif word.nil?
-        my_each { |elt| return false if elt.nil? || elt == false }
-      elsif !arg.nil? && (arg.is_a? Class)
-        my_each { |elt| return false if elt.class != arg }
-      elsif !arg.nil? && arg.class == Regexp
-        my_each { |elt| return false unless arg.match(elt) }
-      else
-        my_each { |elt| return false if elt != arg }
+      my_each do |elt|
+        return false unless yield(elt) == true
       end
+      true
+    elsif arg.nil?
+      my_each { |elt| return false if elt.nil? || elt == false }
+    elsif !arg.nil? && (arg.is_a? Class)
+      my_each do |elt|
+        return true unless elt.instance_of?(arg.class)
+      end
+      false
+    elsif !arg.nil? && arg.instance_of?(Regexp)
+      my_each { |elt| return false unless elt.match(arg) }
       true
     end
   end
 
-  def my_any?(word=nil)
+  def my_any?(arg = nil)
     if block_given?
-        my_each do |elt|
-            return true unless yield(elt) == false
-        end
-        false
-    elsif word.nil?
-        my_each { |elt| return true if elt.nil? || elt == true }
+      my_each { |elt| return true if yield(elt) }
+      false
+    elsif arg.nil?
+      my_each { |elt| return true if elt.nil? || elt == true }
     elsif !arg.nil? && (arg.is_a? Class)
-        my_each { |elt| return true if elt.class != arg }
-    elsif !arg.nil? && arg.class == Regexp
-        my_each { |elt| return true unless arg.match(elt) }
+      my_each { |elt| return true unless elt.class != arg }
+      false
+    elsif !arg.nil? && (arg.instance_of? Regexp)
+      my_each { |elt| return true if arg.match(elt) }
+      false
     else
-        my_each { |elt| return true if elt != arg }
+      my_each { |elt| return true if elt == arg }
     end
     false
-end
+  end
 
-def my_none?
+  def my_none?(arg = nil)
     return to_enum(:my_none) unless block_given?
-    
-    arr = self
-    index = 0
-    my_each do |elt|
-    if (yield(elt) == false) && (index < arr.length)
-    index += 1
-    elsif yield(elt) == true
-    return false
-    end
-    return true
-end
-def my_count(number = nil)
-    arr = self
-    new_arr = []
-    if block_given? && number==nil
-      new_arr<<arr.to_a.my_select { |elt| elt == yield(elt) }
-    elsif number
-      arr.my_select { |elt| elt == number }.length
-    else
-      arr.length
+
+    if block_given?
+      my_each { |elt| return false if yield(elt) }
+      true
+    elsif !arg.nil? && (arg.instance_of? Regexp)
+      my_each { |elt| return false if elt.match(arg) }
+      true
+    elsif !arg.nil? && (arg.is_a? Class)
+      my_each { |elt| return false unless elt.instance_of?(arg) }
+      true
     end
   end
+
+  def my_count(number = nil, &block)
+    arr = to_a
+    return arr.length unless block_given? || number
+
+    return arr.my_select { |elt| elt == number }.length if number
+
+    arr.my_select(&block).length
+  end
+
   def my_map(proc = nil)
     return to_enum(:my_map) unless block_given? || proc.nil?
-    
+
     arr = self
     new_arr = []
     if proc.nil?
-    arr.my_each { |elt| new_arr << yield(elt) }
+      arr.my_each { |elt| new_arr << yield(elt) }
     else
-    arr.to_a.my_each { |elt| new_arr << proc.call(elt) }
+      arr.to_a.my_each { |elt| new_arr << proc.call(elt) }
     end
     new_arr
-    end 
+  end
 end
+
 def my_inject(num = nil, sym = nil)
   if block_given?
     memo = num
     my_each do |elt|
       memo = memo.nil? ? elt : yield(memo, elt)
     end
-   memo
+    memo
   elsif !num.nil? && (num.is_a?(Symbol) || num.is_a?(String))
-   memo = nil
+    memo = nil
     my_each do |elt|
-     memo =memo.nil? ? elt :memo.send(num, elt)
+      memo = memo.nil? ? elt : memo.send(num, elt)
     end
-   memo
+    memo
   elsif !sym.nil? && (sym.is_a?(Symbol) || sym.is_a?(String))
-   memo = num
+    memo = num
     my_each do |elt|
-     memo =memo.nil? ? elt :memo.send(sym, elt)
+      memo = memo.nil? ? elt : memo.send(sym, elt)
     end
-   memo
+    memo
+
   end
 end
 [1, 2, 3, 4].my_each do |element|
   puts element
 end
 [3, 1, 3, 2].my_each_with_index do |element, index|
-  print element, index
+  puts element
+  print index
 end
 (1..4).my_each_with_index do |element, index|
-  print element, index
+  puts element
+  print index
+  puts ''
 end
+p [1, 2, 3, 4, 5].my_select(&:even?)
+p1 = %w[ant bear cat].my_any? { |wor| wor.length >= 3 }
+p2 = [nil, true, 99].my_any?(Integer)
+p3 = %w[ant bear cat].my_any?(/bos/)
+p4 = %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+p5 = [1, 2i, 3.14].my_all?(Numeric)
+p6 = %w[ant bear cat].my_all?(/t/)
+p7 = [2, 3, 56, 6, 3, 2, 9, 1, 2, 3, 3, 5].my_count
+p8 = (0..5).my_count(2)
+p9 = %w[ant bear cat].my_none? { |word| word.length == 5 }
+p10 = %w[ant bear cat].my_none? { |word| word.length >= 4 }
+puts p1
+puts p2
+puts p3
+puts p4
+puts p5
+puts p6
+puts p7
+puts p8
+puts p9
+puts p10

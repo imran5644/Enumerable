b17696fa5921fe7df235114c995711dc5dd04b17
che30 <che.blancoo58@gmail.com>
2020-11-16 20:23:52 +0100

    complete my_each method

--- spec/Enumerable_spec.rb
+++ spec/Enumerable_spec.rb
@@ -10,9 +10,15 @@ describe Enumerable do
       it {expect(hash.my_each).to be_instance_of(Enumerator)}
       it {expect(arr.my_each {|x| puts x*2}).not_to eql([2, 4, 6])}
       it {expect(arr.my_each {|x| puts x*2}).to be_instance_of(Array)}
+      it {expect(range.my_each{|x| puts x*2}).not_to be_instance_of(Array)}
+      it {expect(hash.my_each{|x| puts x*2}).not_to be_instance_of(Array)}
+      it {expect{arr.my_each(1){|x| puts x}}.to raise_error(ArgumentError)}
+      it { expect { |x| arr.my_each(&x) }.to yield_control }
+      specify { expect { print(arr.my_each) }.to output.to_stdout }
 
-      it {expect(hash.my_each{|x| x=={'x'=>'a', 'y'=>'b'} })}
-      it {expect(range.my_each {|x| x==(1...5)})}
+
+      # it {expect(hash.my_each{|x| x=={'x'=>'a', 'y'=>'b'} })}
+      # it {expect(range.my_each {|x| x==(1...5)})}
 
     end
   end

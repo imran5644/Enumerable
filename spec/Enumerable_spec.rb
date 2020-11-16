a9d2e54d8e379181191bfcae2bcb2b23312f448e
che30 <che.blancoo58@gmail.com>
2020-11-16 19:47:05 +0100

    Add one new test case for array

--- spec/Enumerable_spec.rb
+++ spec/Enumerable_spec.rb
@@ -9,6 +9,8 @@ describe Enumerable do
       it{ expect(arr.my_each).to be_instance_of(Enumerator)}
       it {expect(hash.my_each).to be_instance_of(Enumerator)}
       it {expect(arr.my_each {|x| puts x*2}).not_to eql([2, 4, 6])}
+      it {expect(arr.my_each {|x| puts x*2}).to be_instance_of(Array)}
+
       it {expect(hash.my_each{|x| x=={'x'=>'a', 'y'=>'b'} })}
       it {expect(range.my_each {|x| x==(1...5)})}
 
